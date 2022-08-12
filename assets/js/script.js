4c8b186a7079318e299464cc27301bf7c52b566e
M Taylor <107767318+mtaylo1@users.noreply.github.com>
2022-08-11 22:04:50 -0400

    edits to html, js, css

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ var getCurrentWeather = (event) => {
 
         var iconcode = [0].icon;
 
-        var currentWeatherBox = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        var currentWeatherBox="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         let currentTimeUTC = response.dt;
         let currentTimeZoneOffset = response.timezone;
         let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
